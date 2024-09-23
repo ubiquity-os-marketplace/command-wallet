a9ca1c235f74d38735573184b273bc259e8e251a
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-23 20:05:27 +0900

    chore: fixed errors

--- src/handlers/validator.ts
+++ src/handlers/validator.ts
@@ -1,15 +1,14 @@
-import { Value } from "@sinclair/typebox/value";
+import { Value, ValueError } from "@sinclair/typebox/value";
 import { Env, envValidator, PluginSettings, pluginSettingsSchema, pluginSettingsValidator } from "../types";
 
 export function validateAndDecodeSchemas(env: Env, rawSettings: object) {
   const settings = Value.Default(pluginSettingsSchema, rawSettings) as PluginSettings;
 
   if (!pluginSettingsValidator.test(settings)) {
-    const errorDetails: object[] = [];
+    const errorDetails: ValueError[] = [];
     for (const error of pluginSettingsValidator.errors(settings)) {
-      const errorMessage = { path: error.path, message: error.message, value: error.value };
-      console.error(errorMessage);
-      errorDetails.push(errorMessage);
+      console.error(error);
+      errorDetails.push(error);
     }
     return new Response(JSON.stringify({ message: `Bad Request: the settings are invalid.`, errors: errorDetails }), {
       status: 400,
@@ -20,11 +19,10 @@ export function validateAndDecodeSchemas(env: Env, rawSettings: object) {
   const decodedSettings = Value.Decode(pluginSettingsSchema, settings);
 
   if (!envValidator.test(env)) {
-    const errorDetails: object[] = [];
+    const errorDetails: ValueError[] = [];
     for (const error of envValidator.errors(env)) {
-      const errorMessage = { path: error.path, message: error.message, value: error.value };
-      console.error(errorMessage);
-      errorDetails.push(errorMessage);
+      console.error(error);
+      errorDetails.push(error);
     }
     return new Response(JSON.stringify({ message: `Bad Request: the environment is invalid.`, errors: errorDetails }), {
       status: 400,
