f2f7fdf61e18497a00d359c421b16a3d202ada5a
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-09-23 20:51:15 +0900

    chore: updated errors

--- src/handlers/validator.ts
+++ src/handlers/validator.ts
@@ -2,35 +2,25 @@ import { Value, ValueError } from "@sinclair/typebox/value";
 import { Env, envValidator, PluginSettings, pluginSettingsSchema, pluginSettingsValidator } from "../types";
 
 export function validateAndDecodeSchemas(env: Env, rawSettings: object) {
+  const errors: ValueError[] = [];
   const settings = Value.Default(pluginSettingsSchema, rawSettings) as PluginSettings;
 
   if (!pluginSettingsValidator.test(settings)) {
-    const errorDetails: ValueError[] = [];
     for (const error of pluginSettingsValidator.errors(settings)) {
       console.error(error);
-      errorDetails.push(error);
+      errors.push(error);
     }
-    return new Response(JSON.stringify({ message: `Bad Request: the settings are invalid.`, errors: errorDetails }), {
-      status: 400,
-      headers: { "content-type": "application/json" },
-    });
   }
 
-  const decodedSettings = Value.Decode(pluginSettingsSchema, settings);
-
   if (!envValidator.test(env)) {
-    const errorDetails: ValueError[] = [];
     for (const error of envValidator.errors(env)) {
       console.error(error);
-      errorDetails.push(error);
+      errors.push(error);
     }
-    return new Response(JSON.stringify({ message: `Bad Request: the environment is invalid.`, errors: errorDetails }), {
-      status: 400,
-      headers: { "content-type": "application/json" },
-    });
   }
 
   const decodedEnv = Value.Decode(envValidator.schema, env);
+  const decodedSettings = Value.Decode(pluginSettingsSchema, settings);
 
-  return { decodedEnv, decodedSettings };
+  return { decodedEnv, decodedSettings, errors };
 }
--- src/worker.ts
+++ src/worker.ts
@@ -15,9 +15,12 @@ export default {
         } else if (request.method === "POST") {
           const webhookPayload = await request.json();
 
-          const result = validateAndDecodeSchemas(env, webhookPayload.settings);
-          if (result instanceof Response) {
-            return result;
+          const { errors } = validateAndDecodeSchemas(env, webhookPayload.settings);
+          if (errors.length) {
+            return new Response(JSON.stringify({ message: `Bad Request: the settings are invalid.`, errors }), {
+              status: 400,
+              headers: { "content-type": "application/json" },
+            });
           }
           return new Response(JSON.stringify({ message: "Schema is valid" }), { status: 200, headers: { "content-type": "application/json" } });
         }
@@ -38,14 +41,17 @@ export default {
 
       const webhookPayload = await request.json();
 
-      const result = validateAndDecodeSchemas(env, webhookPayload.settings);
+      const { errors, decodedSettings, decodedEnv } = validateAndDecodeSchemas(env, webhookPayload.settings);
 
-      if (result instanceof Response) {
-        return result;
+      if (errors.length) {
+        return new Response(JSON.stringify({ message: `Bad Request: the settings are invalid.`, errors }), {
+          status: 400,
+          headers: { "content-type": "application/json" },
+        });
       }
 
-      webhookPayload.settings = result.decodedSettings;
-      await plugin(webhookPayload, env);
+      webhookPayload.settings = decodedSettings;
+      await plugin(webhookPayload, decodedEnv);
       return new Response(JSON.stringify("OK"), { status: 200, headers: { "content-type": "application/json" } });
     } catch (error) {
       return handleUncaughtError(error);
@@ -53,8 +59,11 @@ export default {
   },
 };
 
-function handleUncaughtError(error: unknown) {
-  console.error(error);
+function handleUncaughtError(errors: unknown) {
+  console.error(errors);
   const status = 500;
-  return new Response(JSON.stringify({ error }), { status: status, headers: { "content-type": "application/json" } });
+  return new Response(JSON.stringify({ errors: Array.isArray(errors) ? errors : [errors] }), {
+    status: status,
+    headers: { "content-type": "application/json" },
+  });
 }
