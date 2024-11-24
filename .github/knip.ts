b7bf02693a0e0284533c2994ebb7092f750bb50a
whilefoo <whilefoo@users.noreply.github.com>
2024-11-24 17:26:12 +0100

    chore: upgrade sdk

--- .github/knip.ts
+++ .github/knip.ts
@@ -6,7 +6,7 @@ const config: KnipConfig = {
   ignore: ["**/__mocks__/**", "**/__fixtures__/**", "src/types/database.ts"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@mswjs/data", "ts-node"],
+  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@mswjs/data", "ts-node", "hono"],
   eslint: true,
 };
 
