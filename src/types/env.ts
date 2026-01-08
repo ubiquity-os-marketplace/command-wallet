5759d66a3fc508ae75787ac8deae6e8d66624568
0x4007 <gpg@pavlovcik.com>
2026-01-09 06:42:51 +0900

    chore: merge latest from origin

--- src/types/env.ts
+++ src/types/env.ts
@@ -5,6 +5,7 @@ export const envSchema = T.Object({
   SUPABASE_URL: T.String(),
   SUPABASE_KEY: T.String(),
   KERNEL_PUBLIC_KEY: T.Optional(T.String()),
+  UOS_AI_TOKEN: T.Optional(T.String()),
   LOG_LEVEL: T.Optional(T.String()),
 });
 
