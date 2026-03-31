06a008fe008c24b46a9cdb468ba3c98db8021021
gentlementlegen <fernand.veyrier@epitech.eu>
2026-03-31 19:31:20 +0900

    fix: update JSON import syntax

--- src/worker.ts
+++ src/worker.ts
@@ -1,15 +1,14 @@
+import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
+import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import type { ExecutionContext } from "hono";
+import manifest from "../manifest.json" with { type: "json" };
 import { createAdapters } from "./adapters/index";
+import { plugin } from "./plugin";
+import { Command } from "./types/command";
 import { SupportedEvents } from "./types/context";
 import { Env, envSchema } from "./types/env";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
-import { Command } from "./types/command";
-import { plugin } from "./plugin";
-import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
-import { LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-
-const manifest = (await import("../manifest.json")).default;
 
 export default {
   async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
